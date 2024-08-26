862162c05c1a82cfabc723f35acce664518432e3
CodingWithDash <codingwithdash@gmail.com>
2024-08-26 19:42:11 +0530

    text input color changed

--- src/screens/chatbot/style.ts
+++ src/screens/chatbot/style.ts
@@ -19,6 +19,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 5,
     paddingHorizontal: 10,
+    color: '#000',
   },
   microphoneButton: {
     backgroundColor: '#f00',
